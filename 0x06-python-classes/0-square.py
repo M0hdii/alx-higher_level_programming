c9a677a5097e04b93c788dd6b5ea4951c26205d1
M0hdii <mehdiomar71@gmail.com>
2023-12-19 13:21:41 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+    This module is a simple implementation
+    of a class, and how to proparly use
+    docstrings
+"""
+
+
 class Square:
     """An empty class that defines a square
 
