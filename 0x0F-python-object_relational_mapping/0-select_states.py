10c0c382e467cfab635e817e1c4268516f494265
M0hdii <mehdiomar71@gmail.com>
2024-03-16 17:45:45 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    # Check if all three arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute SQL query to fetch all states sorted by states.id
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print results
     for row in rows:
         print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
