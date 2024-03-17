e6a662594de255e86908c1637b7c7aada20c5a78
M0hdii <mehdiomar71@gmail.com>
2024-03-16 18:15:29 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the database `hbtn_0e_0_usa`.
-"""
+"""Lists states"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(argv[0]))
-        exit(1)
-
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    database_name = argv[3]
-
-    try:
-        db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row)
-
-        cur.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
